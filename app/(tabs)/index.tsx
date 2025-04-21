1514504a8012b469977beef12a0c300e3d2f4725
akanksha <akankshathube2108@gmail.com>
2025-04-21 09:02:50 +0530

    signIn Page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,12 @@ import Splash from "@/src/screens/auth/Splash/index";
 import SignUp from "@/src/screens/auth/SignUp/signup";
 import { Image, StyleSheet, Platform, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import SignIn from "@/src/screens/auth/SignIn/signIn";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
-      <SignUp />
+      <SignIn />
     </SafeAreaView>
   );
 }
