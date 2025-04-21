dd93918ad07645ef7e40d0efca6fd2ad8dc804ef
akanksha <akankshathube2108@gmail.com>
2025-04-21 14:59:44 +0530

    Navigation sof screen with stack and normal

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,39 @@
 import Splash from "@/src/screens/auth/Splash/index";
 import SignUp from "@/src/screens/auth/SignUp/signup";
-import { Image, StyleSheet, Platform, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
 import SignIn from "@/src/screens/auth/SignIn/signIn";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Stack = createNativeStackNavigator();
 
 export default function HomeScreen() {
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
-      <SignIn />
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#ffffff",
+          },
+          headerTitleAlign: "center",
+          headerTintColor: "#000000",
+        }}
+      >
+        <Stack.Screen
+          name="Splash"
+          component={Splash}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignUp"
+          component={SignUp}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="SignIn"
+          component={SignIn}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
     </SafeAreaView>
   );
 }
